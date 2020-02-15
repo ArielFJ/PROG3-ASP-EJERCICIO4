a2c28dbf21ae81698fef658de9263b84ea6c1594
Ariel <arielfj@magnanym.com>
2020-02-15 16:01:26 -0400

    Apartado Trigonometría completo, actualización general de las vistas

---
+++ Ejercicio4/Controllers/TrigonometriaController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ejercicio4.Helper;
+using Ejercicio4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ejercicio4.Controllers
+{
+    public class TrigonometriaController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Seno()
+        {
+            return View(new OperandosViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Seno(OperandosViewModel model)
+        {
+            model.Num2 = 0;
+            return View(FuncTrigonometria.DeterminarOperacion(model, OperacionesTrigonometria.Seno));
+        }
+
+        public IActionResult Coseno()
+        {
+            return View(new OperandosViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Coseno(OperandosViewModel model)
+        {
+            model.Num2 = 0;
+            return View(FuncTrigonometria.DeterminarOperacion(model, OperacionesTrigonometria.Coseno));
+        }
+
+        public IActionResult Tangente()
+        {
+            return View(new OperandosViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Tangente(OperandosViewModel model)
+        {
+            model.Num2 = 0;
+            return View(FuncTrigonometria.DeterminarOperacion(model, OperacionesTrigonometria.Tangente));
+        }
+
+        public IActionResult Cotangente()
+        {
+            return View(new OperandosViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Cotangente(OperandosViewModel model)
+        {
+            model.Num2 = 0;
+            return View(FuncTrigonometria.DeterminarOperacion(model, OperacionesTrigonometria.Cotangente));
+        }
+
+        public IActionResult Secante()
+        {
+            return View(new OperandosViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Secante(OperandosViewModel model)
+        {
+            model.Num2 = 0;
+            return View(FuncTrigonometria.DeterminarOperacion(model, OperacionesTrigonometria.Secante));
+        }
+
+        public IActionResult Cosecante()
+        {
+            return View(new OperandosViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Cosecante(OperandosViewModel model)
+        {
+            model.Num2 = 0;
+            return View(FuncTrigonometria.DeterminarOperacion(model, OperacionesTrigonometria.Cosecante));
+        }
+    }
+}
---
+++ Ejercicio4/Helper/FuncTrigonometria.cs
@@ -0,0 +1,57 @@
+﻿using Ejercicio4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio4.Helper
+{
+
+    public enum OperacionesTrigonometria
+    {
+        Seno,
+        Coseno,
+        Tangente,
+        Cotangente,
+        Secante,
+        Cosecante
+    }
+
+    public class FuncTrigonometria
+    {
+
+        public static OperandosViewModel DeterminarOperacion(OperandosViewModel model, OperacionesTrigonometria op)
+        {
+            model.Resultado = null;
+            if (model.Num1.HasValue && model.Num2.HasValue)
+            {
+                double grados = (model.Num1.Value * Math.PI) / 180;
+                switch (op)
+                {
+                    case OperacionesTrigonometria.Seno:
+                        model.Resultado = Math.Sin(grados);
+                        break;
+                    case OperacionesTrigonometria.Coseno:
+                        model.Resultado = Math.Cos(grados);
+                        break;
+                    case OperacionesTrigonometria.Tangente:
+                        model.Resultado = Math.Tan(grados);
+                        break;
+                    case OperacionesTrigonometria.Cotangente:
+                        model.Resultado = 1 / Math.Tan(grados);
+                        break;
+                    case OperacionesTrigonometria.Secante:
+                        model.Resultado = 1 / Math.Cos(grados);
+                        break;
+                    case OperacionesTrigonometria.Cosecante:
+                        model.Resultado = 1 / Math.Sin(grados);
+                        break;
+                }
+                model.Resultado = (Math.Round((double)model.Resultado * 10000)) / 10000;
+                return model;
+            }
+            return model;
+        }
+
+    }
+}
