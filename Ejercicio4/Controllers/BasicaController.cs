7c2805f2fa872b7df4939cab34989bfe61208e18
Ariel <arielfj@magnanym.com>
2020-02-15 12:27:56 -0400

    Vista básica creada, modelo para la vista creado

---
+++ Ejercicio4/Controllers/BasicaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ejercicio4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ejercicio4.Controllers
+{
+    public class BasicaController : Controller
+    {       
+        public IActionResult Suma()
+        {
+            return View(new OperandosViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Suma(OperandosViewModel model)
+        {
+            model.Resultado = null;
+            if (ModelState.IsValid)
+            {
+                model.Resultado = model.Num1 + model.Num2;
+                return View(model);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Ejercicio4/Models/OperandosViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio4.Models
+{
+    public class OperandosViewModel
+    {
+        [Required]
+        [Display(Name = "Número 1")]
+        public double Num1 { get; set; }
+
+        [Display(Name = "Número 2")]
+        [Required]
+        public double Num2 { get; set; }
+
+
+        public double? Resultado { get; set; }
+    }
+}
